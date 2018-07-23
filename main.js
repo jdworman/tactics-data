fe0b4118d6f8f76354a4d63164e1e45cacc08f23
Joseph Worman <jdworman84@gmail.com>
2018-07-23 17:18:56 -0400

    debugged

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function getClothes() {
      if (this.readyState == 4 && this.status == 200) {
        var myClothes = JSON.parse(this.responseText);
        console.log(myClothes);
+
        document.getElementById("t-shirt-name").innerHTML = 'The t-shirt cost ' + t-shirt.cost;
        document.getElementById("pants-hp").innerHTML = 'The pants cost ' + pants.cost;
        document.getElementById("hat-attack").innerHTML = 'The hat cost ' + hat.cost;
