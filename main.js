36667eb9ec2f9a1aeef76ed7766ee26e3d928f9c
Joseph Worman <jdworman84@gmail.com>
2018-07-23 11:41:05 -0400

    minor changes

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
-function loadClothes() {
+function getClothes() {
   var xhttp = new XMLHttpRequest();
   console.log(xhttp); {
     if (this.readyState == 4 && this.status == 200) {
       var myClothes = JSON.parse(this.responseText);
       document.getElementById('info').src = myObj.sprites.front_default;
       console.log(this.responseText);
-    }yorvi
+    }
     };
     xhttp.open("GET","https://github.com/jdworman/tactics-data.git", true);
     console.log(xhttp);
