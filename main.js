58ff5fa99f9d54a1ca2a17dde3a9583b0f126fc0
Joseph Worman <jdworman84@gmail.com>
2018-07-23 17:06:41 -0400

    added get elements

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ function getClothes() {
      if (this.readyState == 4 && this.status == 200) {
        var myClothes = JSON.parse(this.responseText);
        console.log(myClothes);
+       document.getElementById("t-shirt-name").innerHTML = 'The t-shirt cost ' + t-shirt.cost;
+       document.getElementById("pants-hp").innerHTML = 'The pants cost ' + pants.cost;
+       document.getElementById("hat-attack").innerHTML = 'The hat cost ' + hat.cost;
+       document.getElementById("socks-defense").innerHTML = 'The socks cost ' + socks.cost;
+       document.getElementById("shoes-ability").innerHTML = 'The shoes cost ' + shoes.cost;
+       document.getElementById("belt-ability").innerHTML = 'The belt cost ' + belt.cost;
      }
    };
    xhttp.open('GET','https://raw.githubusercontent.com/jdworman/tactics-data/master/tactics.json', true);
