6d41778fe9852f5eab0cfa942007b05a5e2a26ff
Joseph Worman <jdworman84@gmail.com>
2018-07-23 15:37:40 -0400

    debug

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 function getClothes() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystagechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var myClothes = JSON.parse(this.responseText);
-      console.log(myClothes);
-    }
-    };
-    xhttp.open('GET','https://raw.githubusercontent.com/jdworman/tactics-data/master/tactics.json', true);
-    xhttp.send();
-  }
+ var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+     if (this.readyState == 4 && this.status == 200) {
+       var myClothes = JSON.parse(this.responseText);
+       console.log(myComic.company);
+     }
+   };
+   xhttp.open('GET','https://raw.githubusercontent.com/jdworman/tactics-data/master/tactics.json', true);
+   xhttp.send();
+   }
+
+// function getClothes() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystagechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       var myClothes = JSON.parse(this.responseText);
+//       console.log(myClothes);
+//     }
+//     };
+//     xhttp.open('GET','https://raw.githubusercontent.com/jdworman/tactics-data/master/tactics.json', true);
+//     xhttp.send();
+//   }
