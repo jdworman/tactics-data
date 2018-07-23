853f90f63c0012ffb394a65133146c43ba7934c0
Joseph Worman <jdworman84@gmail.com>
2018-07-23 16:34:13 -0400

    add images

--- main.js
+++ main.js
@@ -8,16 +8,4 @@ function getClothes() {
    };
    xhttp.open('GET','https://raw.githubusercontent.com/jdworman/tactics-data/master/tactics.json', true);
    xhttp.send();
-   }
-
-// function getClothes() {
-//   var xhttp = new XMLHttpRequest();
-//   xhttp.onreadystagechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//       var myClothes = JSON.parse(this.responseText);
-//       console.log(myClothes);
-//     }
-//     };
-//     xhttp.open('GET','https://raw.githubusercontent.com/jdworman/tactics-data/master/tactics.json', true);
-//     xhttp.send();
-//   }
+ }
