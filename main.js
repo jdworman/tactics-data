7aa2d4eba073870f7962999de558a41303002967
Joseph Worman <jdworman84@gmail.com>
2018-07-23 15:32:17 -0400

    json added

--- main.js
+++ main.js
@@ -3,10 +3,9 @@ function getClothes() {
   xhttp.onreadystagechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var myClothes = JSON.parse(this.responseText);
-      console.log(myClothes.clothes);
-
+      console.log(myClothes);
     }
     };
-    xhttp.open("GET","https://raw.githubusercontent.com/jdworman/tactics-data/master/tactics.json", true);
+    xhttp.open('GET','https://raw.githubusercontent.com/jdworman/tactics-data/master/tactics.json', true);
     xhttp.send();
   }
